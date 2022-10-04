6a4866bf0c6f3c72ada0e215a34a80904604190e
KncskBenedek <koncsikbenedek@gmail.com>
2022-10-04 14:06:14 +0200

    MeetingController kész

--- meeting/app/Http/Controllers/MeetingController.php
+++ meeting/app/Http/Controllers/MeetingController.php
@@ -9,24 +9,35 @@ use Illuminate\Http\Request;
 class MeetingController extends Controller
 {
     public function show($id){
-        $meeting = response()->json(Meeting::find($id));
+        return response()->json(Meeting::find($id));
+        
     }
     public function edit(Response $response, $id){
-
+        $meeting = Meeting::find($id);
+        $meeting->date = $response->date;
+        $meeting->location = $response->location;
+        $meeting->max_number = $response->max_number;
+        $meeting->save();
     }
     public function new(Response $response){
-
+        $meeting = new Meeting();
+        $meeting->date = $response->date;
+        $meeting->location = $response->location;
+        $meeting->max_number = $response->max_number;
+        $meeting->save();
     }
     public function delete($id){
-        
+        Meeting::find($id)->delete();
     }
     public function listView(){
-        
+        $meetings = Meeting::all();
+        return view('meeting.list', ['meetings'=> $meetings]);
     }
     public function editView($id){
-
+        $meeting = Meeting::find($id);
+        return view('meeting.edit', ['meeting'=> $meeting]);
     }
     public function newView(){
-
+        return view('meeting.new');
     }
 }
